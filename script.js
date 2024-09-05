32ba2099841228b677482ae5bcb85eaa7791767d
mohammed kaif <mkaif555123@gmail.com>
2024-09-06 00:42:35 +0500

    100% Work Completed

--- script.js
+++ script.js
@@ -5,29 +5,56 @@ const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    // console.log(input.value);
-
     if (input.value === "") {
         Swal.fire({
             title: 'Oops',
             text: 'Please Add Your Text',
             icon: 'error',
-        })
-    }
-    else {
+        });
+    } else {
         const li = document.createElement("li");
         ul.appendChild(li);
-        li.className = "li"
-        li.innerHTML = input.value
-        const span = document.createElement("span");
-        span.className = "span"
-        span.innerHTML = "\u00d7"
-        li.appendChild(span)
+        li.className = "li";
 
-        span.addEventListener("click" , () =>{
-          li.remove()
-        })
+        const editSpan = document.createElement("span");
+        editSpan.className = "icon edit";
+        editSpan.innerHTML = "&#9998;";
+        li.appendChild(editSpan);
 
+        const deleteSpan = document.createElement("span");
+        deleteSpan.className = "icon delete";
+        deleteSpan.innerHTML = "&#10005;";
+        li.appendChild(deleteSpan);
+
+        const textSpan = document.createElement("span");
+        textSpan.className = "text";
+        textSpan.innerText = input.value;
+        li.appendChild(textSpan);
+
+        deleteSpan.addEventListener("click", () => {
+            li.remove();
+        });
+
+        editSpan.addEventListener("click", () => {
+            const editInput = document.createElement("input");
+            editInput.type = "text";
+            editInput.value = textSpan.innerText;
+            textSpan.replaceWith(editInput);
+            editInput.focus();
+
+            editInput.addEventListener("blur", () => {
+                if (editInput.value.trim() !== "") {
+                    textSpan.innerText = editInput.value;
+                }
+                editInput.replaceWith(textSpan);
+            });
+
+            editInput.addEventListener("keydown", (e) => {
+                if (e.key === "Enter") {
+                    editInput.blur();
+                }
+            });
+        });
     }
-    form.reset()
-})
+    form.reset();
+});
